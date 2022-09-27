9467e79e2d0a40caaece052de2027793a189a7d2
Melisa Zvala <melisa.zvala@gmail.com>
2022-09-27 20:25:10 -0300

    conocido+

--- M3U4/app.js
+++ M3U4/app.js
@@ -69,11 +69,10 @@ app.post('/ingresar', function (req, res) {
 });
 
 
-app.get('/salir', function (req,res) {
+app.get('/salir', function (req, res) {
   req.session.destroy();
-  res.redirect('/')
-}
-);
+  res.redirect('/');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
