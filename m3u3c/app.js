aadda1afe5cccd70f174ea63314d62604ac23cbb
melisa zvala <melisa.zvala@gmail.com>
2022-10-30 10:53:26 -0300

    proyecto

--- m3u3c/app.js
+++ m3u3c/app.js
@@ -8,6 +8,9 @@ var logger = require('morgan');
 require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/admin/login');
+var adminRouter = require('./routes/admin/novedades');
+
 
 var app = express();
 
@@ -23,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use ('/admin/login', loginRouter);
+app.use('/admin/novedades', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
