d0b398191edddc433f047954dcf2bc78b432105b
melisa zvala <melisa.zvala@gmail.com>
2022-10-31 19:32:55 -0300

    crud2check

--- m3u3c/routes/admin/novedades.js
+++ m3u3c/routes/admin/novedades.js
@@ -26,7 +26,11 @@ router.get('./../views/admin/novedades')
     res.redirect("/admin/novedades")
   });
 
-
+router.get('/agregar', (req,res, next) =>{
+  res.render('admin/agregar', {
+    layout: 'admin/layout'
+  });
+});
 
   async function deleteNovedadById(id) {
     var query = 'delete from novedades where id = ?';
@@ -35,5 +39,56 @@ router.get('./../views/admin/novedades')
 
   }
  
+router.post('/agregar', async (req, res, next) =>{
+  try {
+    if (req.body.titulo != "" && req.body.subtitulo != "" &&
+    req.body.cuerpo !="") {
+      await novedadesModel.insertNovedad(req.body);
+      res.redirect('/admin/novedades')
+    } else {
+      res.render('/admin/agregar', {
+        layout: 'admin/layout',
+        error: true, message: 'Todos los campos son requeridos'
+      })
+    }
+  } catch (error){
+    console.log(error)
+    res.render('admin/agregar', {
+      layout: 'admin/layout',
+      error: true, message: 'No se cargo la novedad'
+    });
+  }
+});
+
+
+router.get('/modificar/:id', async (req, res, next) =>{
+
+  let id = req.params.id;
+  let novedad = await novedadesModel.getNovedadById(id);
+  res.render('admin/modificar', {
+    layout: 'admin/layout', 
+    novedad
+  });
+});
+
+router.post('/modificar', async (req, res, next) => {
+  try {
+    let obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
+    }
+
+    await novedadesModel.modificarNovedadById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+  }
+  catch (error) {
+    console.log(error)
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true, message: 'No se pudo modificar la novedad'
+    })
+  }
+})
 
 module.exports = router
