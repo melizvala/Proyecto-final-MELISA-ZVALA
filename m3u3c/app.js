8e333b7e15f371c0986a9513742d228adec20484
melisa zvala <melisa.zvala@gmail.com>
2022-10-31 07:07:20 -0300

    typeerror

--- m3u3c/app.js
+++ m3u3c/app.js
@@ -3,8 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 var session = require('express-session');
-
 var pool = require('./models/bd');
 
 
